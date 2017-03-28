a287910df2a68894d9eda4397182b9838e9b7037
leejean <ilove_cp3@163.com>
2017-03-28 23:31:39 +0800

    springboot教程
    第一次提交

---
+++ Chapter1/src/main/java/com/didispace/Chapter1Application.java
@@ -0,0 +1,20 @@
+package com.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * http://git.oschina.net/leejean/SpringBoot-Learning
+ * TODO 在此写上类的相关说明.<br>
+ * @author Leejean <br>
+ * @version 1.0.0 2017年3月28日 下午4:19:50<br>
+ * @see 
+ * @since JDK 1.7.0
+ */
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+
+}
---
+++ Chapter3-1-5/src/main/java/com/didispace/Application.java
@@ -0,0 +1,22 @@
+package com.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		//http://127.0.0.1:8080/swagger-ui.html
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
