801e3480b2db8ec61376d92885ca83885db96726
leejean <ilove_cp3@163.com>
2017-04-01 18:51:24 +0800

    缓存支持：注解配置与EhCache使用
    缓存支持：注解配置与EhCache使用

--- Chapter4-4-1/src/test/java/com/didispace/ApplicationTests.java
+++ Chapter4-4-1/src/test/java/com/didispace/ApplicationTests.java
@@ -29,22 +29,30 @@ public class ApplicationTests {
 
 	@Before
 	public void before() {
-		userRepository.save(new User("AAA", 10));
+		
 	}
 
 	@Test
 	public void test() throws Exception {
 
-		User u1 = userRepository.findByName("AAA");
-		System.out.println("第一次查询：" + u1.getAge());
-
-		User u2 = userRepository.findByName("AAA");
-		System.out.println("第二次查询：" + u2.getAge());
-
-		u1.setAge(20);
-		userRepository.save(u1);
-		User u3 = userRepository.findByName("AAA");
-		System.out.println("第三次查询：" + u3.getAge());
+		try {
+			//userRepository.save(new User("AAA", 10));
+			System.out.println("--------------------------------------"+System.currentTimeMillis());
+			User u1 = userRepository.findByName("AAA");
+			System.out.println("第一次查询：" + u1.getAge());
+			System.out.println("--------------------------------------"+System.currentTimeMillis());
+			User u2 = userRepository.findByName("AAA");
+			System.out.println("第二次查询：" + u2.getAge());
+			System.out.println("--------------------------------------"+System.currentTimeMillis());
+			u1.setAge(20);
+			userRepository.save(u1);
+			User u3 = userRepository.findByName("AAA");
+			System.out.println("第三次查询：" + u3.getAge());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
 
 	}
 
