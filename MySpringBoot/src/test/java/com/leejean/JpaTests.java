feadf4885d71d38c9345db590b02e4da7234e2e3
leejean <ilove_cp3@163.com>
2017-04-25 18:37:43 +0800

    邮件发送

--- MySpringBoot/src/test/java/com/leejean/JpaTests.java
+++ MySpringBoot/src/test/java/com/leejean/JpaTests.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
---
+++ MySpringBoot/src/test/java/com/leejean/MailTests.java
@@ -0,0 +1,113 @@
+package com.leejean;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.velocity.app.VelocityEngine;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.ui.velocity.VelocityEngineUtils;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.Map;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class MailTests {
+
+	@Autowired
+	private JavaMailSender mailSender;
+	@Autowired
+	private VelocityEngine velocityEngine;
+
+	@Test
+	public void sendSimpleMail() throws Exception {
+
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setFrom("ilove_cp3@163.com");
+			message.setTo("ilove_cp3@163.com");
+			message.setSubject("主题：简单邮件");
+			message.setText("测试邮件内容");
+			mailSender.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void sendAttachmentsMail() throws Exception {
+
+		try {
+			MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+			helper.setFrom("ilove_cp3@163.com");
+			helper.setTo("ilove_cp3@163.com");
+			helper.setSubject("主题：有附件");
+			helper.setText("有附件的邮件");
+
+			FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+			helper.addAttachment("附件-1.jpg", file);
+			helper.addAttachment("附件-2.jpg", file);
+
+			mailSender.send(mimeMessage);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+	}
+
+	@Test
+	public void sendInlineMail() throws Exception {
+
+		try {
+			MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+			helper.setFrom("ilove_cp3@163.com");
+			helper.setTo("ilove_cp3@163.com");
+			helper.setSubject("主题：嵌入静态资源");
+			helper.setText("<html><body><img src=\"cid:weixin\" ></body></html>", true);
+
+			FileSystemResource file = new FileSystemResource(new File("weixin.jpg"));
+			helper.addInline("weixin", file);
+
+			mailSender.send(mimeMessage);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+	}
+
+	@Test
+	public void sendTemplateMail() throws Exception {
+
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+		helper.setFrom("ilove_cp3@163.com");
+		helper.setTo("ilove_cp3@163.com");
+		helper.setSubject("主题：模板邮件");
+
+		Map<String, Object> model = new HashedMap();
+		model.put("username", "didi");
+		String text = VelocityEngineUtils.mergeTemplateIntoString(
+				velocityEngine, "template.vm", "UTF-8", model);
+		helper.setText(text, true);
+
+		mailSender.send(mimeMessage);
+	}
+
+}
