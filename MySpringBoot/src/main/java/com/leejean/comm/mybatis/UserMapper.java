f3bb0b918fb72f7f9f8d1d8f9124312048414570
leejean <ilove_cp3@163.com>
2017-04-25 15:31:57 +0800

    添加mybatis作为数据源

---
+++ MySpringBoot/src/main/java/com/leejean/comm/mybatis/UserMapper.java
@@ -0,0 +1,16 @@
+package com.leejean.comm.mybatis;
+
+import org.apache.ibatis.annotations.*;
+
+import com.leejean.entitys.User;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM t_user WHERE name = #{name}")
+    User findByName(@Param("name") String name);
+
+    @Insert("INSERT INTO t_user(name, age) VALUES(#{name}, #{age})")
+    int insert(@Param("name") String name, @Param("age") Integer age);
+
+}
---
+++ MySpringBoot/src/test/java/com/leejean/MybatisTests.java
@@ -0,0 +1,35 @@
+package com.leejean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.leejean.comm.mybatis.UserMapper;
+import com.leejean.entitys.User;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+//@Transactional
+public class MybatisTests {
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+//	@Rollback
+	public void insert() throws Exception {
+		int i = userMapper.insert("消消暑", 15);
+		System.out.println(i);
+	}
+	@Test
+//	@Rollback
+	public void findByName() throws Exception {
+		User u = userMapper.findByName("消消暑");
+		System.out.println(u);
+	}
+
+}
