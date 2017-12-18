a91a403457989f45a1bff53df4eb0ec7f263daf6
易理坚 <ilove_cp3@163.com>
2017-12-18 10:39:50 +0800

    add

---
+++ springboot-leejean/src/main/java/cn/leejean/springbootleejean/SpringbootLeejeanApplication.java
@@ -0,0 +1,12 @@
+package cn.leejean.springbootleejean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLeejeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLeejeanApplication.class, args);
+	}
+}
---
+++ springboot-leejean/src/test/java/cn/leejean/springbootleejean/SpringbootLeejeanApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.leejean.springbootleejean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootLeejeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
